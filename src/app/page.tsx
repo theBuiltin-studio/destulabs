a4b7197a90ed9d76794b931f494077c0b2178bd1
Abhishekraj-ivoyant <abhishekraj88731@gmail.com>
2024-02-24 20:16:15 +0530

    start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import ComingSoon from "./components/ComingSoon";
 export default function Home() {
   return (
-    <div className="container">
-    <ComingSoon />
-  </div>
+    <div>
+      
+    </div>
   );
 }
